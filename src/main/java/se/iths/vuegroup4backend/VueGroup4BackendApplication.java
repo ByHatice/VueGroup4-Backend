e28798eb7c7c618a31ee516b2170be2a9be4054e
Hanna Stigland <hanna.stigland@iths.se>
2024-10-03 14:12:28 +0200

    Initial commit

---
+++ src/main/java/se/iths/vuegroup4backend/VueGroup4BackendApplication.java
@@ -0,0 +1,13 @@
+package se.iths.vuegroup4backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueGroup4BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueGroup4BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/vuegroup4backend/VueGroup4BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.vuegroup4backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueGroup4BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
